1d6f5c3e7f05c2589dedce6721b60f29e0898bcd
MKashanButt <kashanbutt566@gmail.com>
2024-04-04 02:41:38 +0500

    updated route

--- app/Filament/Resources/LeadResource.php
+++ app/Filament/Resources/LeadResource.php
@@ -127,7 +127,7 @@ class LeadResource extends Resource
     {
         return [
             'index' => Pages\ListLeads::route('/'),
-            'create' => Pages\CreateLead::route('/create'),
+            'create' => Pages\ListLeads::route('/'),
             'edit' => Pages\EditLead::route('/{record}/edit'),
         ];
     }
