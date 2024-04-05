e79ee994a235b4d9c3b4d428ec02fc7c0fb3dbab
MKashanButt <kashanbutt566@gmail.com>
2024-04-06 00:11:32 +0500

    added copy option and edit fields

--- app/Filament/Resources/LeadResource.php
+++ app/Filament/Resources/LeadResource.php
@@ -27,6 +27,17 @@ class LeadResource extends Resource
     {
         return $form
             ->schema([
+                Forms\Components\Select::make('status')
+                    ->options([
+                        'Denied' => 'Denied',
+                        'Error' => 'Error',
+                        'Payable' => 'Payable',
+                        'Approved' => 'Approved',
+                        'Wrong Doc' => 'Wrong Doc',
+                        'Paid' => 'Paid',
+                        'Awaiting' => 'Awaiting'
+                    ])
+                    ->required(),
                 Forms\Components\Select::make('transfer_status')
                     ->options([
                         'Transferred' => 'Transferred',
@@ -34,6 +45,79 @@ class LeadResource extends Resource
                         'Awaiting' => 'Awaiting'
                     ])
                     ->required(),
+                Forms\Components\Select::make('center_code_id')
+                    ->relationship('centerCode', 'code')
+                    ->searchable()
+                    ->preload()
+                    ->noSearchResultsMessage('No Center Found')
+                    ->required(),
+                Forms\Components\Select::make('insurance_id')
+                    ->relationship('insurance', 'insurance')
+                    ->required(),
+                Forms\Components\Select::make('products_id')
+                    ->relationship('products', 'products')
+                    ->required(),
+                Forms\Components\TextInput::make('patient_phone')
+                    ->tel()
+                    ->required()
+                    ->maxLength(15),
+                Forms\Components\TextInput::make('secondary_phone')
+                    ->tel()
+                    ->maxLength(15)
+                    ->default(null),
+                Forms\Components\TextInput::make('first_name')
+                    ->required()
+                    ->maxLength(15),
+                Forms\Components\TextInput::make('last_name')
+                    ->required()
+                    ->maxLength(15),
+                Forms\Components\DatePicker::make('dob')
+                    ->required(),
+                Forms\Components\TextInput::make('medicare_id')
+                    ->required()
+                    ->maxLength(15),
+                Forms\Components\Textarea::make('address')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('city')
+                    ->required()
+                    ->maxLength(15),
+                Forms\Components\TextInput::make('state')
+                    ->required()
+                    ->maxLength(15),
+                Forms\Components\TextInput::make('zip')
+                    ->required()
+                    ->maxLength(15),
+                Forms\Components\Textarea::make('product_specs')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('doctor_name')
+                    ->required()
+                    ->maxLength(30),
+                Forms\Components\TextInput::make('facility_name')
+                    ->required()
+                    ->maxLength(20),
+                Forms\Components\TextInput::make('patient_last_visit')
+                    ->required()
+                    ->maxLength(20),
+                Forms\Components\Textarea::make('doctor_address')
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('doctor_phone')
+                    ->tel()
+                    ->required()
+                    ->maxLength(15),
+                Forms\Components\TextInput::make('doctor_fax')
+                    ->required()
+                    ->maxLength(20),
+                Forms\Components\TextInput::make('doctor_npi')
+                    ->required()
+                    ->maxLength(50),
+                Forms\Components\Textarea::make('recording_link')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\Textarea::make('comments')
+                    ->required()
+                    ->columnSpanFull(),
             ]);
     }
 
@@ -43,52 +127,73 @@ class LeadResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('created_at')
                     ->date()
+                    ->copyable()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('status')
                     ->badge('status')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('transfer_status')
                     ->badge('status')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('centerCode.code')
                     ->numeric()
+                    ->copyable()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('insurance.insurance')
                     ->numeric()
+                    ->copyable()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('products.products')
                     ->numeric()
+                    ->copyable()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('patient_phone')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('secondary_phone')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('first_name')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('last_name')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('dob')
+                    ->copyable()
                     ->date()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('medicare_id')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('city')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('state')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('zip')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('doctor_name')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('facility_name')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('patient_last_visit')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('doctor_phone')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('doctor_fax')
+                    ->copyable()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('doctor_npi')
+                    ->copyable()
                     ->searchable()
             ])
             ->filters([
